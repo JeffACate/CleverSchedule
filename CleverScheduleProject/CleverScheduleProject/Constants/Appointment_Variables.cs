1a40313287b38470ec570a9a3a3459338e05ac41
jeff Cate <Jeff.a.cate@gmail.com>
2020-04-22 10:27:59 -0500

    Appointment constants added

---
+++ CleverScheduleProject/CleverScheduleProject/Constants/Appointment_Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleverScheduleProject.Constants
+{
+    public static class Appointment_Variables
+    {
+        public static string Approved = "Approved";
+        public static string Denied = "Denied";
+        public static string Pending = "Pending";
+    }
+}
